03e40ba1e390720312bc6c6d85d71cae7641f88a
ben0588 <energy95272z@gmail.com>
2024-03-10 05:16:03 +0800

    feat(front-home) add go to top button

---
+++ src/components/front/home/HomeToTopBtn.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home-to-top-container" @click="handleToTop">
+    <button type="button" class="btn btn-none">
+      <font-awesome-icon :icon="['fas', 'chevron-up']" />
+    </button>
+  </div>
+</template>
+<script setup>
+const handleToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+};
+</script>
+<style lang="scss">
+.home-to-top-container {
+  position: fixed;
+  right: 25px;
+  bottom: 25px;
+  background: #fff;
+  border: 1px solid #dfe6e9;
+  box-shadow: 0.5px 0px 5px 0.5px #c3c3c3;
+}
+</style>
--- src/views/front/home/FrontView.vue
+++ src/views/front/home/FrontView.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="d-flex flex-column min-vh-100 px-0" :style="{ maxWidth: `100%`, width: `100%` }">
+  <div
+    class="d-flex flex-column min-vh-100 position-relative px-0"
+    :style="{ maxWidth: `100%`, width: `100%` }"
+  >
     <HeaderComponent></HeaderComponent>
     <main class="flex-grow-1">
       <router-view></router-view>
     </main>
     <FooterComponent></FooterComponent>
+    <HomeToTopBtn />
   </div>
 </template>
 
 <script setup>
 import HeaderComponent from '@/components/layout/HeaderComponent.vue';
 import FooterComponent from '@/components/layout/FooterComponent.vue';
+import HomeToTopBtn from '@/components/front/home/HomeToTopBtn.vue';
 </script>
